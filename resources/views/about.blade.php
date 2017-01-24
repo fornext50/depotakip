89616e88cf1bb794b6ce96723136dcee2b3b7011
Erhan TÜTÜNCÜ <fornext50@gmail.com>
2017-01-24 17:06:53 +0300

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,11 @@
 @section('title','Program')
 @section('content')
 	<div class="row">
-		
+		<h3>Depo Malzeme Takip</h3>
+		<ul>
+			<li>Malzeme Kayıt Sistemi</li>
+			<li>Malzeme Çıkış Sistemi</li>
+			<li>Loglama Sistemi</li>
+		</ul>
 	</div>
 @endsection
