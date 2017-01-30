392d674fb3e490752388090dde942c6c37782028
Erhan TÜTÜNCÜ <fornext50@gmail.com>
2017-01-30 17:19:04 +0300

    Program Versiyon

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,5 +8,9 @@
 			<li>Malzeme Çıkış Sistemi</li>
 			<li>Loglama Sistemi</li>
 		</ul>
+		<ul>
+			<li>Malzeme Emanet Sistemi</li>
+			<li>Malzeme Grup Tanımları</li>
+		</ul>
 	</div>
 @endsection
